b0e165ecfa847c8b2570402945712e56d2c10014
Avadhut Lohar <loharavdhoot@gmail.com>
2023-04-01 12:39:38 +0530

    Created Sidebar

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,26 +1,37 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { toggleMenu } from "../utils/appSlice";
 
 const Head = () => {
+  const dispatch = useDispatch();
+
+  const toggleMenuHandler = () => {
+    dispatch(toggleMenu());
+  };
   return (
     <div className="grid grid-flow-col p-5 m-2 shadow-lg">
       <div className="flex col-span-1 ">
         <img
-          className="w-10 h-10"
+        onClick={() => toggleMenuHandler()}
+
+          className=" h-10 cursor-pointer"
           src="https://cdn-icons-png.flaticon.com/512/3917/3917215.png"
           alt="menu"
         />
+        <a href="/">
         <img
           className=" h-10 mx-2"
           src="https://fontmeme.com/permalink/230401/575c11984cbb2ddcc391d880e8ea9a76.png"
           alt="logo"
         />
+        </a>
       </div>
       <div className="col-span-10 text-center">
         <input
           className="w-1/2 border border-gray-400 p-2 rounded-l-full "
           type="text"
         />
-        <button className="border border-gray-400 px-2 py-2 rounded-r-full bg-gray-100 ">
+        <button className="border border-gray-400 px-2 py-2 rounded-r-full bg-gray-200 ">
           Search
         </button>
       </div>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import store from "../utils/store";
 
 const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
-}
+  const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
 
-export default Sidebar
+  if(!isMenuOpen) return null;
+   
+  return (
+    <div className="p-2 shadow-lg col-span-1">
+      <ul>
+        <li>Home</li>
+        <li>Shorts</li>
+        <li>Subscriptions</li>
+        <li>Live</li>
+      </ul>
+      <h1 className="font-bold pt-5">Subscriptions</h1>
+      <ul>
+        <li>Home</li>
+        <li>Music</li>
+        <li>Sports</li>
+        <li>games</li>
+      </ul>
+      <h1 className="font-bold pt-5">Watch Later</h1>
+      <ul>
+        <li>Home</li>
+        <li>Music</li>
+        <li>Sports</li>
+        <li>games</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
