042a98ba4e780285b24364b32906d6aafae09ebc
Avadhut Lohar <loharavdhoot@gmail.com>
2023-04-07 12:21:49 +0530

    Created WatchPage

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { YOUTUBE_VIDEOS_API } from "../utils/constants";
 import VideoCard from "./VideoCard";
+import { Link } from "react-router-dom";
 
 const VideoContainer = () => {
   const [videos, setVideos] = useState([]);
@@ -12,13 +13,18 @@ const VideoContainer = () => {
   const getVideos = async () => {
     const data = await fetch(YOUTUBE_VIDEOS_API);
     const json = await data.json();
-  
+
     setVideos(json.items);
   };
-  return <div className="flex flex-wrap">
-  {videos.map((video) => <VideoCard key={video.id} info ={video}/>)}
-  
-  </div>;
+  return (
+    <div className="flex flex-wrap">
+      {videos.map((video) => (
+        <Link to={"/watch?v="+ video.id}>
+          <VideoCard key={video.id} info={video} />
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default VideoContainer;
